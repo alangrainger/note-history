28a0c535b8743266dc460dd7ec961d262263ee0b
Alan Grainger <alan.grainger@gmail.com>
2024-08-16 00:57:01 +0200

    SQL.js

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,23 @@ export default class MyPlugin extends Plugin {
   async onload () {
     await this.loadSettings()
 
+    const initSqlJs = require('sql.js');
+// or if you are in a browser:
+// const initSqlJs = window.initSqlJs;
+
+    const SQL = await initSqlJs({
+      // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
+      // You can omit locateFile completely when running in node
+      locateFile: file => `sql-wasm.wasm`
+    });
+    const db = new SQL.Database();
+    let sqlstr = "CREATE TABLE hello (a int, b char); \
+INSERT INTO hello VALUES (0, 'hello'); \
+INSERT INTO hello VALUES (1, 'world');";
+    db.run(sqlstr); // Run the query without returning anything
+
+
+
     // This adds a settings tab so the user can configure various aspects of the plugin
     this.addSettingTab(new MySettingTab(this.app, this))
   }
